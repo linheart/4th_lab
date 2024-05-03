e5d12db195bf188ba4426e8dbd18bf47b39d0312
Alexei <leshamihaylenko@bk.ru>
2024-05-03 19:51:24 +0700

    ex5 C++

--- C++/5th_ex/main.cpp
+++ C++/5th_ex/main.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <cstddef>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -8,7 +10,7 @@ using namespace std;
 
 void err();
 vector<double> init_arr(int bound);
-vector<double> fib(vector<double> arr, int a, int b);
+void fib(vector<double> &arr, int a, int b);
 
 int main() {
   int a, b;
@@ -21,10 +23,14 @@ int main() {
     err();
 
   vector<double> arr = init_arr(a);
-  vector<double> res = fib(arr, a, b);
-
-  for (auto it : res)
+  for (auto it : arr)
     cout << it << ' ';
+  cout << endl;
+
+  fib(arr, a, b);
+
+  for (size_t i = a; i < arr.size(); i++)
+    cout << arr[i] << ' ';
 
   return 0;
 }
@@ -39,20 +45,16 @@ vector<double> init_arr(int bound) {
   random_device rd;
   mt19937 gen(rd());
   uniform_real_distribution<double> unif(0., 1.);
-  for (int i = 0; i <= bound; i++)
-    arr.push_back(unif(gen));
-
+  for (int i = 0; i < bound; i++)
+    arr.push_back(round(unif(gen) * 10) / 10);
   return arr;
 }
 
-vector<double> fib(vector<double> arr, int a, int b) {
-  vector<double> res = {};
-  for (int i = a + 1; i <= series + a; i++) {
+void fib(vector<double> &arr, int a, int b) {
+  for (int i = a; i < series + a; i++) {
     double tmp = arr[i - a] - arr[i - b];
     if (tmp < 0)
       tmp++;
     arr.push_back(tmp);
-    res.push_back(tmp);
   }
-  return res;
 }
